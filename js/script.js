7b56f04ffd79e3c6211ef43238c5706f3ecb8963
cheyanna <cheyanna.graham@mgmail.com>
2019-09-28 20:48:40 -0700

    change button id to next

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const addEvents = () => {
+    let next_button = document.getElementById("next");
+}
+
+
+//add events after the dom is ready
+document.addEventListener("DOMContentLoaded",addEvents);
