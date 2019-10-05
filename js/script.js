78aafc1728e3577895c4d68afae454e33179b948
cheyanna <cheyanna.graham@mgmail.com>
2019-10-04 21:54:03 -0700

    hide/show prev/next form step

--- js/script.js
+++ js/script.js
@@ -3,27 +3,22 @@ const formValues = {};
 
 const main = () => {
 	addEvents(formValues);
-	// getForms().then(res => {
-	// 	formParts = res;
-	// 	// Get first part of form
-	// 	updateFormView(0);
-	// });
 };
 
 const addEvents = () => {
 	const prevBtn = document.getElementById("prev-btn");
 	prevBtn.addEventListener("click", () => {
 		if (isValid()) {
-			const prev = document.querySelector("fieldset[data-prev]").getAttribute("data-prev");
-			updateFormView(prev);
+			// const prev = document.querySelector("fieldset[data-prev]").getAttribute("data-prev");
+			changeStep("prev");
 		}
 	});
 
 	const nextBtn = document.getElementById("next-btn");
 	nextBtn.addEventListener("click", () => {
 		if (isValid()) {
-			const next = document.querySelector("fieldset[data-next]").getAttribute("data-next");
-			updateFormView(next);
+			// const next = document.querySelector("fieldset[data-next]").getAttribute("data-next");
+			changeStep("next");
 		}
 	});
 
@@ -39,61 +34,62 @@ const addEvents = () => {
 };
 
 const isValid = e => {
+	return true;
 	// Validate onchange element
-	if (e) {
-		elem = e.target;
-		if (!elem.checkValidity()) {
-			elem.reportValidity();
-			elem.classList.add("invalid");
-			return false;
-		} else {
-			elem.classList.remove("invalid");
-			return true;
-		}
-	} else {
-		// Validate all step elements on next/prev btn click
-		const formContent = document.getElementById("form-content");
+	// if (e) {
+	// 	elem = e.target;
+	// 	if (!elem.checkValidity()) {
+	// 		elem.reportValidity();
+	// 		elem.classList.add("invalid");
+	// 		return false;
+	// 	} else {
+	// 		elem.classList.remove("invalid");
+	// 		return true;
+	// 	}
+	// } else {
+	// 	// Validate all step elements on next/prev btn click
+	// 	const formContent = document.getElementById("form-content");
 
-		// Validate all checkbox groups
-		const checkboxes = [...formContent.querySelectorAll("input[type=checkbox]")];
-		if (checkboxes.length > 0) {
-			const checkbox_groups_names = [...new Set(Object.values(checkboxes).map(box => box.name)).values()];
-			const checkbox_group_validity = [];
+	// 	// Validate all checkbox groups
+	// 	const checkboxes = [...formContent.querySelectorAll("input[type=checkbox]")];
+	// 	if (checkboxes.length > 0) {
+	// 		const checkbox_groups_names = [...new Set(Object.values(checkboxes).map(box => box.name)).values()];
+	// 		const checkbox_group_validity = [];
 			
-			// iterate through check box groups
-			checkbox_groups_names.forEach((group_name, index) => {
-				checkbox_group_validity[index] = false;
+	// 		// iterate through check box groups
+	// 		checkbox_groups_names.forEach((group_name, index) => {
+	// 			checkbox_group_validity[index] = false;
 
-				// get boxes for single group by name
-				const boxes = checkboxes.filter(box => box.name == group_name);
+	// 			// get boxes for single group by name
+	// 			const boxes = checkboxes.filter(box => box.name == group_name);
 
-				// remove groups with atleast 1 checked value
-				checkbox_group_validity[index] = boxes.some(box => box.checked);
-			});
+	// 			// remove groups with atleast 1 checked value
+	// 			checkbox_group_validity[index] = boxes.some(box => box.checked);
+	// 		});
 			
-			// Report validity error
-			if (checkbox_group_validity.includes(false)) {
-				document.getElementById("error").innerHTML = "Please select at least 1 checkbox";
-				return false;
-			} else {
-				document.getElementById("error").innerHTML = " ";
-			}
-		}
+	// 		// Report validity error
+	// 		if (checkbox_group_validity.includes(false)) {
+	// 			document.getElementById("error").innerHTML = "Please select at least 1 checkbox";
+	// 			return false;
+	// 		} else {
+	// 			document.getElementById("error").innerHTML = " ";
+	// 		}
+	// 	}
 
-		// Validate select & required
-		let stepValid = true;
-		const stepItems = formContent.querySelectorAll("input[required], select[required], textarea[required");
-		stepItems.forEach(elem => {
-			if (!elem.checkValidity()) {
-				elem.reportValidity();
-				elem.classList.add("invalid");
-				stepValid = false;
-			} else {
-				elem.classList.remove("invalid");
-			}
-		});
-		return stepValid;
-	}
+	// 	// Validate select & required
+	// 	let stepValid = true;
+	// 	const stepItems = formContent.querySelectorAll("input[required], select[required], textarea[required");
+	// 	stepItems.forEach(elem => {
+	// 		if (!elem.checkValidity()) {
+	// 			elem.reportValidity();
+	// 			elem.classList.add("invalid");
+	// 			stepValid = false;
+	// 		} else {
+	// 			elem.classList.remove("invalid");
+	// 		}
+	// 	});
+	// 	return stepValid;
+	// }
 };
 
 // const getForms = () => {
@@ -116,70 +112,70 @@ const isValid = e => {
 // };
 
 const saveFormContent = e => {
-	elem = e.target;
+	// elem = e.target;
 	
-	//   Save Radio & checkboxes
-	if (["radio", "checkbox"].includes(elem.type)) {
-		if (elem.checked) {
+	// //   Save Radio & checkboxes
+	// if (["radio", "checkbox"].includes(elem.type)) {
+	// 	if (elem.checked) {
 			
-			// Uncheck other radio buttons
-			if (elem.type == "radio") {
-				const radios = document.querySelectorAll(`input[name=${elem.name}]`);
-				radios.forEach(item => {
-					item.removeAttribute("checked");
-				});
-			}
-			elem.setAttribute("checked", "");
-		} else {
-			elem.removeAttribute("checked");
-		}
+	// 		// Uncheck other radio buttons
+	// 		if (elem.type == "radio") {
+	// 			const radios = document.querySelectorAll(`input[name=${elem.name}]`);
+	// 			radios.forEach(item => {
+	// 				item.removeAttribute("checked");
+	// 			});
+	// 		}
+	// 		elem.setAttribute("checked", "");
+	// 	} else {
+	// 		elem.removeAttribute("checked");
+	// 	}
 		
-		// Save single selects
-	} else if (elem.type == "select-one") {
-		const options = document.querySelectorAll(`#${elem.id} option`);
-		options.forEach(item => {
-			if (item.value == elem.value) {
-				item.setAttribute("selected", elem.selected);
-			} else {
-				item.removeAttribute("selected");
-			}
-		});
+	// 	// Save single selects
+	// } else if (elem.type == "select-one") {
+	// 	const options = document.querySelectorAll(`#${elem.id} option`);
+	// 	options.forEach(item => {
+	// 		if (item.value == elem.value) {
+	// 			item.setAttribute("selected", elem.selected);
+	// 		} else {
+	// 			item.removeAttribute("selected");
+	// 		}
+	// 	});
 		
-		// Save textarea's
-	} else if (elem.type == "textarea") {
-		document.getElementById(elem.id).innerHTML = e.target.value;
+	// 	// Save textarea's
+	// } else if (elem.type == "textarea") {
+	// 	document.getElementById(elem.id).innerHTML = e.target.value;
 		
-		// save other inputs
-	} else {
-		elem.setAttribute("value", elem.value);
-	}
+	// 	// save other inputs
+	// } else {
+	// 	elem.setAttribute("value", elem.value);
+	// }
 
-	const currentForm = document.querySelector("fieldset[data-current]").getAttribute("data-current");
-	currentContent = document.getElementById("form-content").innerHTML;
-	formParts[currentForm] = currentContent;
+	// const currentForm = document.querySelector("fieldset[data-current]").getAttribute("data-current");
+	// currentContent = document.getElementById("form-content").innerHTML;
+	// formParts[currentForm] = currentContent;
 };
 
-const updateFormView = form => {
-	const formContent = document.getElementById("form-content");
-	formContent.innerHTML = formParts[form];
+const changeStep = direction => {
+	const current_step = document.querySelector("div.form-step.show");
+	const moveTo = direction == 'next' ? current_step.nextElementSibling : current_step.previousElementSibling;
+	
+	current_step.classList.remove("show");
+	moveTo.classList.add("show");
+	console.log(moveTo.getAttribute("data-position") == "beg");
 
-	const prevBtn = document.getElementById("prev-btn");
-	const nextBtn = document.getElementById("next-btn");
-	const submitBtn = document.getElementById("submit-btn");
-
-	// Hide prev button at beg of form only
-	if (form == "0") {
-		prevBtn.style.display = "none";
-	} else {
-		prevBtn.style.display = "inline";
+	if (moveTo.getAttribute("data-position") != "beg") {
+		document.getElementById('prev-btn').style.display = 'inline';
 	}
-	// Show submit on last page of form only
-	if (form == "4") {
-		nextBtn.style.display = "none";
-		submitBtn.style.display = "inline";
-	} else {
-		nextBtn.style.display = "inline";
-		submitBtn.style.display = "none";
+	else {
+		document.getElementById('prev-btn').style.display = 'none';
+	}
+	if (moveTo.getAttribute("data-position") == "end") {
+		document.getElementById('submit-btn').style.display = 'inline';
+		document.getElementById('next-btn').style.display = 'none';
+	}
+	else {
+		document.getElementById('submit-btn').style.display = 'none';
+		document.getElementById('next-btn').style.display = 'inline';
 	}
 };
 
