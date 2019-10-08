5bae083c44b41191005051ffbb8c6c92f710fb8c
cheyanna <cheyanna.graham@mgmail.com>
2019-10-07 21:42:21 -0700

    save form data in storage api

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-// let formParts = [];
-// const formValues = {};
-
 const main = () => {
 	addEvents();
 };
@@ -25,7 +22,7 @@ const addEvents = () => {
 	});
 
 	form.addEventListener("change", e => {
-		isValid(e) && saveFormContent(e);
+		isValid(e);
 	});
 };
 
@@ -42,126 +39,6 @@ const isValid = e => {
 			return true;
 		}
 	}));
-
-
-	// Validate onchange element
-	// if (e) {
-	// 	elem = e.target;
-	// 	if (!elem.checkValidity()) {
-	// 		elem.reportValidity();
-	// 		elem.classList.add("invalid");
-	// 		return false;
-	// 	} else {
-	// 		elem.classList.remove("invalid");
-	// 		return true;
-	// 	}
-	// } else {
-	// 	// Validate all step elements on next/prev btn click
-	// 	const formContent = document.getElementById("form-content");
-
-	// 	// Validate all checkbox groups
-	// 	const checkboxes = [...formContent.querySelectorAll("input[type=checkbox]")];
-	// 	if (checkboxes.length > 0) {
-	// 		const checkbox_groups_names = [...new Set(Object.values(checkboxes).map(box => box.name)).values()];
-	// 		const checkbox_group_validity = [];
-			
-	// 		// iterate through check box groups
-	// 		checkbox_groups_names.forEach((group_name, index) => {
-	// 			checkbox_group_validity[index] = false;
-
-	// 			// get boxes for single group by name
-	// 			const boxes = checkboxes.filter(box => box.name == group_name);
-
-	// 			// remove groups with atleast 1 checked value
-	// 			checkbox_group_validity[index] = boxes.some(box => box.checked);
-	// 		});
-			
-	// 		// Report validity error
-	// 		if (checkbox_group_validity.includes(false)) {
-	// 			document.getElementById("error").innerHTML = "Please select at least 1 checkbox";
-	// 			return false;
-	// 		} else {
-	// 			document.getElementById("error").innerHTML = " ";
-	// 		}
-	// 	}
-
-	// 	// Validate select & required
-	// 	let stepValid = true;
-	// 	const stepItems = formContent.querySelectorAll("input[required], select[required], textarea[required");
-	// 	stepItems.forEach(elem => {
-	// 		if (!elem.checkValidity()) {
-	// 			elem.reportValidity();
-	// 			elem.classList.add("invalid");
-	// 			stepValid = false;
-	// 		} else {
-	// 			elem.classList.remove("invalid");
-	// 		}
-	// 	});
-	// 	return stepValid;
-	// }
-};
-
-// const getForms = () => {
-// 	const getData = n => {
-// 		return fetch(`./forms/form_part_${n}.html`)
-// 			.then(resp => resp.text())
-// 			.then(html => {
-// 				return html;
-// 			});
-// 	};
-
-// 	return Promise.all([
-// 		getData(0),
-// 		getData(1),
-// 		getData(2),
-// 		getData(3),
-// 		getData(4),
-// 		getData(5)
-// 	]).then(forms => forms);
-// };
-
-const saveFormContent = e => {
-	// elem = e.target;
-	
-	// //   Save Radio & checkboxes
-	// if (["radio", "checkbox"].includes(elem.type)) {
-	// 	if (elem.checked) {
-			
-	// 		// Uncheck other radio buttons
-	// 		if (elem.type == "radio") {
-	// 			const radios = document.querySelectorAll(`input[name=${elem.name}]`);
-	// 			radios.forEach(item => {
-	// 				item.removeAttribute("checked");
-	// 			});
-	// 		}
-	// 		elem.setAttribute("checked", "");
-	// 	} else {
-	// 		elem.removeAttribute("checked");
-	// 	}
-		
-	// 	// Save single selects
-	// } else if (elem.type == "select-one") {
-	// 	const options = document.querySelectorAll(`#${elem.id} option`);
-	// 	options.forEach(item => {
-	// 		if (item.value == elem.value) {
-	// 			item.setAttribute("selected", elem.selected);
-	// 		} else {
-	// 			item.removeAttribute("selected");
-	// 		}
-	// 	});
-		
-	// 	// Save textarea's
-	// } else if (elem.type == "textarea") {
-	// 	document.getElementById(elem.id).innerHTML = e.target.value;
-		
-	// 	// save other inputs
-	// } else {
-	// 	elem.setAttribute("value", elem.value);
-	// }
-
-	// const currentForm = document.querySelector("fieldset[data-current]").getAttribute("data-current");
-	// currentContent = document.getElementById("form-content").innerHTML;
-	// formParts[currentForm] = currentContent;
 };
 
 const changeStep = direction => {
@@ -179,48 +56,56 @@ const changeStep = direction => {
 		document.getElementById('prev-btn').style.display = 'none';
 	}
 	if (moveTo.getAttribute("data-position") == "end") {
-		document.getElementById('submit-btn').style.display = 'inline';
+		document.getElementById('submit').style.display = 'inline';
 		document.getElementById('next-btn').style.display = 'none';
 	}
 	else {
-		document.getElementById('submit-btn').style.display = 'none';
+		document.getElementById('submit').style.display = 'none';
 		document.getElementById('next-btn').style.display = 'inline';
 	}
 	if (moveTo.getAttribute('data-position') == 'finish') {
 		document.getElementById('prev-btn').style.display = 'none';
-		document.getElementById('submit-btn').style.display = 'none';
+		document.getElementById('submit').style.display = 'none';
 		document.getElementById('next-btn').style.display = 'none';
 	}
 };
 
 const formSubmit = (form) => {
 	changeStep('next');
-	const formValues = JSON.stringify({
+	const formValues = {
 		name : form.querySelector("#name").value,
 		email : form.querySelector("#email").value,
-		age : form.querySelector("#age").value,
+		age : form.querySelector("#number").value,
 		experience : form.querySelector("#experience").value,
 		focus : Object.values(form.querySelectorAll("input[name=focus]")).filter(elem => elem.checked)[0].value,
 		'tech-stack' : Object.values(form.querySelectorAll("input[name=tech-stack]")).filter(elem => elem.checked).map(elem => elem.value),
-		learn : form.querySelector("#learn").value,
+		learn : form.querySelector("#dropdown").value,
 		comments : form.querySelector("#comments").value,
-	});
-	localStorage.setItem()
-	console.log(formValues);
-	// Object.values(form.elements).forEach(elem => console.log(elem.name, elem.value));
-	// form.elements
-	
-	// console.log(serialize(document.forms('survey-form')));
-	// const formContent = document.getElementById("form-content");
-	// formContent.innerHTML = formParts[5];
-	// document.getElementById("prev-btn").style.display = "none";
-	// document.getElementById("submit-btn").style.display = "none";
+	};
+	saveFormValues(formValues);
+	showResults();
 };
+
+const saveFormValues = (formValues) => {
+	const formResults = JSON.parse(localStorage.getItem("survey-form-results"));
+
+	if (formResults) {
+		formResults.push(formValues);
+		localStorage.setItem("survey-form-results", JSON.stringify(formResults));
+	}
+	else {
+		localStorage.setItem("survey-form-results", JSON.stringify([formValues]));
+	}
+}
+
+const showResults = () => {
+	const formResults = JSON.parse(localStorage.getItem("survey-form-results"));
+
+}
 
 //add events after the dom is ready
 document.addEventListener("DOMContentLoaded", main);
 
 // DONT FORGET TO SANITIZE TEXT!!!
-// retrieve and do something with values
 // prevent values from being empty strings
 
